9c65d5072cf2df317ff799c553fe0f701bd7386d
Kosi Mensah <kmensah@usf.edu>
2024-09-11 22:32:53 -0400

    Initialize inventory with product objects

---
+++ inventory_management_system.js
@@ -0,0 +1,8 @@
+const inventory = [
+ { name: 'Wireless Earbuds', price: 100, quantity: 60, lowStockLevel: 15 },
+ { name: 'Smartwatch', price: 255, quantity: 10, lowStockLevel: 2 },   
+ { name: 'Gaming laptop', price: 1300, quantity: 11, lowStockLevel: 4 },
+{ name: 'Wireless Router', price: 130, quantity: 25, lowStockLevel: 9 },
+{ name: 'Bluetooth Speaker', price: 150, quantity: 25, lowStockLevel: 5 }
+  ];
+  console.log(inventory);
