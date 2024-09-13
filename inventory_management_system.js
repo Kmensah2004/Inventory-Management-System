42479da146074bcfd785237955f3a42bd057a254
Kosi Mensah <kmensah@usf.edu>
2024-09-12 21:18:02 -0400

    Create display ProductDetails function

--- inventory_management_system.js
+++ inventory_management_system.js
@@ -6,3 +6,19 @@ const inventory = [
 { name: 'Bluetooth Speaker', price: 150, quantity: 25, lowStockLevel: 5 }
   ];
   console.log(inventory);
+
+const product = {
+    name: "Smartwatch",
+    price: 255,
+    quantity: 10,
+    lowStockLevel: 2
+};
+function displayProductDetails(product) {
+let stockStatus = product.quantity >= product.lowStockLevel ? "In Stock" : "Low Stock";
+    
+    console.log(`Product Name: ${product.name}`);
+    console.log(`Price: ${product.price}`);
+    console.log(`Quantity in Stock: ${product.quantity}`);
+    console.log(`Stock Status: ${stockStatus}`);
+}
+console.log(displayProductDetails(product));
